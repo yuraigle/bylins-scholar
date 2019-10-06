a511d6747a630926910a77ed64a8591e67934930
yuraigle <yuraigle@gmail.com>
2019-10-06 11:14:11 +0800

    spring starters

---
+++ src/main/java/su/bylins/scholar/ScholarApplication.java
@@ -0,0 +1,13 @@
+package su.bylins.scholar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScholarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScholarApplication.class, args);
+	}
+
+}
